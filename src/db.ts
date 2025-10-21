bdb81a07e042928dfb2bda241b20c9df0f579e6c
MightyPrytanis <davidtowne@gmail.com>
2025-10-21 15:58:18 -0400

    Restore from Archive snapshot 20251021-155818

--- src/db.ts
+++
@@ -1,9 +0,0 @@
-import { drizzle } from 'drizzle-orm/postgres-js';
-import postgres from 'postgres';
-import * as schema from './schema.js';
-
-// Create the connection
-const client = postgres(process.env.DATABASE_URL!);
-
-// Create the database instance
-export const db = drizzle(client, { schema });
